055afeda4532c26b7a215e052d872023a698b872
Guillaume Lederrey <guillaume.lederrey@wikimedia.org>
2021-01-08 11:03:51 +0100

    Add example of using Elastic Common Logging with log4j2

---
+++ backends/backends-log4j2-ecs/src/test/java/org/wikimedia/discovery/lc/logging/backends/log4j2/Log4j2BackendTests.java
@@ -0,0 +1,23 @@
+package org.wikimedia.discovery.lc.logging.backends.log4j2;
+
+import org.apache.logging.log4j.CloseableThreadContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+
+public class Log4j2BackendTests {
+
+    private static final Logger log = LogManager.getLogger(Log4j2BackendTests.class);
+
+    @Test
+    public void warning() {
+        log.warn("Some warning");
+    }
+
+    @Test
+    public void mdc() {
+        try(CloseableThreadContext.Instance ctc = CloseableThreadContext.put("IP", "10.0.0.1")) {
+            log.info("Some info message");
+        }
+    }
+}
